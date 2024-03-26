322bb3defbd93bf71cf00e59d8e530d28a810d84
rogers.machara <rogersmachara13@gmail.com>
2024-03-26 13:00:32 +0300

    modifies objects

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Monroe',
+    streetAddress: 'MELROSE'
+}
+
+function updateEmployeeWithKeyAndValue (object, key, value) {
+    return {...object, [key]: value};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object
+}
+
+function deleteFromEmployeeByKey(object, key, value) {
+    const clonedEmployee = { ...employee };
+    delete clonedEmployee[key];
+    return clonedEmployee
+
+}
+function destructivelyDeleteFromEmployeeByKey(object, key, value){
+    delete object[key]
+    return object
+}
